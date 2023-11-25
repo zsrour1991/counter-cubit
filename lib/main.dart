8a7844254792009a671669ecd1777be89482b68f
zaynab srour <zeinab.srr@hotmail.com>
2023-11-25 18:56:50 +0200

    loading api

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:sizer/sizer.dart';
-import 'package:untitled1/cubit/counter_cubit/cubit.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sizer/sizer.dart';
 import 'package:untitled1/utils/color_schemes.g.dart';
 import 'package:untitled1/utils/constant.dart';
 import 'package:untitled1/utils/custom_route.dart';
-import 'package:untitled1/utils/typography.dart';
+import 'package:untitled1/utils/hive_managment.dart';
 
-Future<void> main() async {
+import 'utils/typography.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox(settingsBox);
@@ -17,45 +18,39 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key,required this.appRouter});
+  const MyApp({super.key, required this.appRouter});
+
   final AppRouter appRouter;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (context) => CounterCubitCubit()),
-        ],
-        child: ValueListenableBuilder(
-            valueListenable: Hive.box(settingsBox).listenable(),
-            builder: (__, box, _) {
-              bool darkMode = box.get(darkModeValue, defaultValue: false);
-              String language = box.get(languageValue, defaultValue: "en");
-              return
-              Sizer(
-              builder: (context, orientation, deviceType)
-              {
-                return MaterialApp.router(
-                  title: nasaNews,
-                  themeMode: darkMode ? ThemeMode.dark : ThemeMode.light,
-                  locale: Locale(language),
-                  supportedLocales: AppLocalizations.supportedLocales,
-                  localizationsDelegates: AppLocalizations
-                      .localizationsDelegates,
-                  theme: ThemeData(
-                      colorScheme: lightColorScheme,
-                      textTheme: textTheme,
-                      useMaterial3: true
-                  ),
-                  darkTheme: ThemeData(
-                      textTheme: textTheme,
-                      useMaterial3: true,
-                      colorScheme: darkColorScheme
-                  ),
-                  routerConfig: appRouter.router,
-                );
-              });
-            }));
+    return ValueListenableBuilder(
+      valueListenable: Hive.box(settingsBox).listenable(),
+      builder: (__, box, _) {
+        bool darkMode = box.get(darkModeValue, defaultValue: false);
+        String language = box.get(languageValue, defaultValue: "en");
+        return Sizer(builder: (context, orientation, deviceType) {
+          return MaterialApp.router(
+            title: nasaNews,
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            locale: Locale(language),
+            themeMode: darkMode ? ThemeMode.dark : ThemeMode.light,
+            theme: ThemeData(
+              colorScheme: lightColorScheme,
+              textTheme: textTheme,
+              useMaterial3: true,
+            ),
+            darkTheme: ThemeData(
+              colorScheme: darkColorScheme,
+              textTheme: textTheme,
+              useMaterial3: true,
+            ),
+            routerConfig: appRouter.router,
+          );
+        });
+      },
+    );
   }
 }
